0617cbd4174e675bc9aab30d6f2f24bb51bba935
LazyPotato <dungngocpham171@gmail.com>
2026-03-24 13:19:36 +0700

    Update solution for entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -3,6 +3,7 @@ import numpy as np
 def entropy_node(y):
     """
     Compute entropy for a single node using stable logarithms.
+    This function use logarithm base 2 not natural log.
     """
     y = np.array(y)
     uniques, counts = np.unique(y, return_counts=True)
