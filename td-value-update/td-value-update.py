595b98471497d9354ce4594ddd68a08439049432
LazyPotato <dungngocpham171@gmail.com>
2026-03-29 13:34:33 +0700

    Update solution for td-value-update

--- td-value-update/td-value-update.py
+++ td-value-update/td-value-update.py
@@ -2,19 +2,7 @@ import numpy as np
 
 def td_value_update(V, s, r, s_next, alpha, gamma):
     """
-    Perform a single TD(0) update.
-    
-    Parameters:
-        V (ndarray): current value function (will be modified in-place)
-        s (int): current state index
-        r (float): reward received
-        s_next (int or None): next state index, None if terminal
-        alpha (float): step size
-        gamma (float): discount factor
-        terminal (bool): whether s_next is terminal
-    
-    Returns:
-        None (V is updated in-place)
+    Returns: updated value function V_new
     """
     target = r + gamma * V[s_next]
     delta = target - V[s]
