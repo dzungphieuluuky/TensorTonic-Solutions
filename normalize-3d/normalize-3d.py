6a0b73242dfd46f292fddfb6477eadf4822f5676
LazyPotato <dungngocpham171@gmail.com>
2026-03-31 11:26:17 +0700

    Update solution for normalize-3d

--- normalize-3d/normalize-3d.py
+++ normalize-3d/normalize-3d.py
@@ -5,8 +5,6 @@ def normalize_3d(v):
     Normalize 3D vector(s) to unit length.
     """
     v = np.array(v)
-    if v.ndim == 1:
-        norm = np.linalg.norm(v)
-    elif v.ndim == 2:
-        norm = np.linalg.norm(v, axis=1, keepdims=True)
+    dims = v.ndim
+    norm = np.linalg.norm(v, axis=dims - 1, keepdims=True)
     return np.where(norm == 0, 0, v / norm)
