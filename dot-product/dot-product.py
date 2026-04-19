bdeaf870dbf38b421047eafa19d2b00c5161ef25
LazyPotato <dungngocpham171@gmail.com>
2026-04-19 22:49:07 +0700

    Update solution for dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -5,8 +5,5 @@ def dot_product(x, y):
     Compute the dot product of two 1D arrays x and y.
     Must return a float.
     """
-    x = np.array(x)
-    y = np.array(y)
-    if x.shape != y.shape:
-        raise ValueError
-    return x @ y
+    # Write code here
+    return np.array(x) @ np.array(y)
