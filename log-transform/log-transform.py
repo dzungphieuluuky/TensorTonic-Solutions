c654740cc49c563b1bbd8a0cc882d068c0eaa1a4
LazyPotato <dungngocpham171@gmail.com>
2026-04-22 14:23:55 +0700

    Update solution for log-transform

--- log-transform/log-transform.py
+++ log-transform/log-transform.py
@@ -2,4 +2,6 @@ def log_transform(values):
     """
     Apply the log1p transformation to each value.
     """
+    # Write code here
+    import numpy as np
     return np.log(1 + np.array(values))
