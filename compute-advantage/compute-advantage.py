bc3fa266891c8c7acff43420010c673c885ae4be
LazyPotato <dungngocpham171@gmail.com>
2026-04-25 21:55:42 +0700

    Update solution for compute-advantage

--- compute-advantage/compute-advantage.py
+++ compute-advantage/compute-advantage.py
@@ -6,8 +6,8 @@ def compute_advantage(states, rewards, V, gamma):
     """
     G = 0
     num_timesteps = len(states)
-    adv = np.zeros(num_timesteps)
+    advantage = np.zeros(num_timesteps)
     for t in range(num_timesteps - 1, -1, -1):
         G = rewards[t] + gamma * G
-        adv[t] = G - V[states[t]]
-    return adv
+        advantage[t] = G - V[states[t]]
+    return advantage
